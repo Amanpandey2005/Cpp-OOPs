94d00609eaaae84c3beba2ca6149744097408553
Aman Pandey <147979955+Amanpandey2005@users.noreply.github.com>
2025-09-30 15:18:55 +0530

    Add files via upload

---
+++ functionoverloading.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+// Function to add two integers
+int add(int a, int b) {
+    return a + b;
+}
+
+// Function to add three integers
+int add(int a, int b, int c) {
+    return a + b + c;
+}
+
+// Function to add two floating point numbers
+double add(double a, double b) {
+    return a + b;
+}
+
+int main() {
+    cout << "Sum of 2 and 3 = " << add(2, 3) << endl;
+    cout << "Sum of 1, 2, and 3 = " << add(1, 2, 3) << endl;
+    cout << "Sum of 2.5 and 3.7 = " << add(2.5, 3.7) << endl;
+
+    return 0;
+}
